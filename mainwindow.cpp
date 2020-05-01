4b6278e16e99ce9378ae4150779a236f5516aea9
Juha Reunanen <juha.reunanen@tomaattinen.com>
2020-05-01 09:47:37 +0300

    Improve the initial progress dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -537,14 +537,10 @@ void MainWindow::openFolder(const QString& dir)
 
     QStringList imageFiles;
 
-    QProgressDialog progress1(tr("Locating image files..."), tr("Stop"), 0, 1, this);
+    QProgressDialog progress1(tr("Locating image files..."), tr("Stop"), 0, 0, this);
     progress1.setMinimumDuration(200);
     progress1.setWindowModality(Qt::WindowModal);
-
-    // For this progress, we don't really know the maximum, so let's not show the bar at all
-    QProgressBar progressBar;
-    progressBar.setVisible(false);
-    progress1.setBar(&progressBar);
+    progress1.show();
 
     auto timeWhenLabelTextLastUpdated = std::chrono::steady_clock::now();
 
@@ -569,7 +565,6 @@ void MainWindow::openFolder(const QString& dir)
         const auto now = std::chrono::steady_clock::now();
         if (now - timeWhenLabelTextLastUpdated >= std::chrono::milliseconds(200)) {
             progress1.setLabelText(tr("Locating image files: %1 found so far").arg(imageFiles.count()) + "\n\n" + filename);
-            progress1.setValue(0);
             QApplication::processEvents(); // update the dialog
             timeWhenLabelTextLastUpdated = now;
         }
